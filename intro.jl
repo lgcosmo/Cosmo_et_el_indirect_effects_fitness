77f04faa097f82e1fdc39b2016b56aeccd49e534
DrWatson <no@mail>
2023-03-03 09:44:18 -0300

    File setup by DrWatson

---
+++ intro.jl
@@ -0,0 +1,16 @@
+using DrWatson
+@quickactivate "Cosmo_et_el_indirect_effects_fitness"
+
+println(
+"""
+Currently active project is: $(projectname())
+
+Path of active project: $(projectdir())
+
+Have fun with your new project!
+
+You can help us improve DrWatson by opening
+issues on GitHub, submitting feature requests,
+or even opening your own Pull Requests!
+"""
+)
